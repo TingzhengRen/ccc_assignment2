3a003f47b21207e4fab2d4192e6972145f725f4b
TingzhengRen <randyrenqaq@gmail.com>
2023-05-17 14:04:16 +1000

    Update sudo, broke line,two geo json api

--- back.py
+++ back.py
@@ -57,7 +57,8 @@ def api_0(param):
                 '$regex': param
                 
             }
-        }
+        },
+        'limit': 200000
     }
     results = []
     # get the results
@@ -82,21 +83,20 @@ db_name2 = 'twitter_vic'
 # get the database
 db2 = couch[db_name2]
 
-@app.route('/api_1/1')
+@app.route('/api_1/city_count')
 def api_1():
     query = {
         'selector': {
             'city': {
-                
-                '$exists': True
-                
+                '$ne': None
             }
-        }
+            
+        },
+        'limit': 200000
     }
     results = []
     for row in db2.find(query):
-        if row['city'] != None:
-            results.append(row['city'])
+        results.append(row['city'])
     count = {}
     for i in range(len(results)):
         if results[i] in count:
@@ -104,6 +104,78 @@ def api_1():
         else:
             count[results[i]] = 1
     return {'data': count}
+
+db_name3 = 'vic_geo'
+# get the database
+db3 = couch[db_name3]
+
+@app.route('/api_2/vic_geo')
+def api_2():
+    query = {
+        "selector": {
+            "_id": {"$gt": None}
+        },
+        'limit': 5000
+    }
+    results = []
+    for row in db3.find(query):
+        results.append((row['properties'],row['geometry']))
+    return {'data': results}
+
+db_name4 = 'city_geo'
+# get the database
+db4 = couch[db_name4]
+
+@app.route('/api_3/city_geo')
+def api_3():
+    query = {
+        "selector": {
+            "_id": {"$gt": None}
+        },
+        'limit': 5000
+    }
+    results = []
+    for row in db4.find(query):
+        results.append((row['properties'],row['geometry']))
+    return {'data': results}
+
+db_name5 = 'sudo'
+# get the database
+db5 = couch[db_name5]
+
+@app.route('/api_4/broke_line')
+def api_4():
+    query = {
+        "selector": {
+            "_id": {"$gt": None}
+        },
+        'limit': 5000
+    }
+    results = []
+    for row in db5.find(query):
+        results.append(row)
+
+    # Extracting keys starting with "f_" and "m_"
+    f_keys_to_extract = []
+    m_keys_to_extract = []
+    for feature in results[0]['features']:
+        properties = feature['properties']
+        f_keys_to_extract.extend(key for key in properties.keys() if 'income' in key and key.startswith('f_'))
+        m_keys_to_extract.extend(key for key in properties.keys() if 'income' in key and key.startswith('m_'))
+
+    # Accessing the values of the extracted keys
+    f_extracted_values = []
+    m_extracted_values = []
+    for feature in results[0]['features']:
+        properties = feature['properties']
+        f_values = [properties[key] for key in f_keys_to_extract]
+        m_values = [properties[key] for key in m_keys_to_extract]
+        f_extracted_values.append(f_values)
+        m_extracted_values.append(m_values)
+
+    return {'data': [f_extracted_values, m_extracted_values]}
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port='8080')
